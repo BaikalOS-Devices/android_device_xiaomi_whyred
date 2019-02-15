2b57342ba6e6f305c2ebe3dd2b3131f1ee77ea11
HeartzStealer <44124557+HeartStealer786@users.noreply.github.com>
2019-02-15 10:26:06 +0530

    Update superior_whyred.mk

--- superior_whyred.mk
+++ superior_whyred.mk
@@ -18,15 +18,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
-$(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/whyred.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
 
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
 # Inherit some common Superior stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/aokp/config/common.mk)
 
-PRODUCT_NAME := superior_whyred
+PRODUCT_NAME := aokp_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
